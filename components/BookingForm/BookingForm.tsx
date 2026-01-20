fdc8c34baba2dbad33478f04bca1b694074f0e52
nasti <anastasiasaienko634@gmail.com>
2026-01-20 21:59:43 +0100

    BookingForm created

--- components/BookingForm/BookingForm.tsx
+++ components/BookingForm/BookingForm.tsx
@@ -1,5 +1,47 @@
+import { Formik, Form, Field } from "formik";
+import css from "./BookingForm.module.css";
+
 const BookingForm = () => {
-  return <div></div>;
+  const handleSubmit = () => {};
+  return (
+    <div className={css.container}>
+      <h3 className={css.bookingTitle}>Book your campervan now</h3>
+      <p className={css.bookingDescription}>
+        Stay connected! We are always ready to help you.
+      </p>
+      <Formik
+        initialValues={{
+          name: "",
+          email: "",
+          bookingDate: "",
+          comment: "",
+        }}
+        onSubmit={handleSubmit}
+      >
+        <Form className={css.bookingForm}>
+          <Field className={css.bookingField} name="name" placeholder="Name*" />
+          <Field
+            className={css.bookingField}
+            name="name"
+            placeholder="Email*"
+          />
+          <Field
+            className={css.bookingField}
+            name="name"
+            placeholder="Booking date*"
+          />
+          <Field
+            className={css.bookingTextarea}
+            name="name"
+            placeholder="Comment"
+            as="textarea"
+            rows="4"
+          />
+          <button className={css.bookingButton}>Send</button>
+        </Form>
+      </Formik>
+    </div>
+  );
 };
 
 export default BookingForm;
