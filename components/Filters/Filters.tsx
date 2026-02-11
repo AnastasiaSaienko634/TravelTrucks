732eaa5477c5b5666ca1cc6e21289d8881aea112
nasti <anastasiasaienko634@gmail.com>
2026-02-11 15:02:08 +0100

    Filter store added

---
+++ components/Filters/Filters.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Filters = () => {
+  return (
+    <>
+      <h3>Filters</h3>
+      <h2>Vehicle equipment</h2>
+    </>
+  );
+};
+
+export default Filters;
--- lib/store/campersStore.ts
+++ lib/store/campersStore.ts
@@ -2,10 +2,38 @@ import { create } from "zustand";
 
 interface FilterStore {
   city: string;
+  equipment: string[];
+  vehicleType: string | null;
+
   setCity: (city: string) => void;
+  toggleEquipment: (item: string) => void;
+  setVehicleType: (type: string) => void;
+  resetFilters: () => void;
 }
 
 export const useFilterStore = create<FilterStore>((set) => ({
   city: "",
+  equipment: [],
+  vehicleType: null,
+
   setCity: (city) => set({ city }),
+
+  toggleEquipment: (item) =>
+    set((state) => ({
+      equipment: state.equipment.includes(item)
+        ? state.equipment.filter((i) => i !== item)
+        : [...state.equipment, item],
+    })),
+
+  setVehicleType: (type) =>
+    set((state) => ({
+      vehicleType: state.vehicleType === type ? null : type,
+    })),
+
+  resetFilters: () =>
+    set({
+      city: "",
+      equipment: [],
+      vehicleType: null,
+    }),
 }));
