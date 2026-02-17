deed11c8bd8d871ff6cf3606bc42ff8c411e36e9
nasti <anastasiasaienko634@gmail.com>
2026-02-17 19:27:15 +0100

    errors fixed

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,3 @@
-import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "../components/Header/Header";
@@ -10,27 +9,8 @@ const inter = Inter({
   variable: "--font-inter",
 });
 
-export const metadata: Metadata = {
-  title: "NoteHub",
-  description: "Create your own notes and you will never forget anything.",
-  openGraph: {
-    title: "Notehub",
-    description: "Create your own notes and you will never forget anything.",
-    url: "https://08-zustand-two-pi.vercel.app",
-    images: [
-      {
-        url: "https://ac.goit.global/fullstack/react/notehub-og-meta.jpg",
-        width: 1200,
-        height: 630,
-        alt: "Notehub",
-      },
-    ],
-  },
-};
-
 export default function RootLayout({
   children,
-  modal,
 }: Readonly<{
   children: React.ReactNode;
   modal: React.ReactNode;
@@ -40,10 +20,7 @@ export default function RootLayout({
       <body className={inter.variable}>
         <TanStackProvider>
           <Header />
-          <main>
-            {modal}
-            {children}
-          </main>
+          <main>{children}</main>
         </TanStackProvider>
       </body>
     </html>
