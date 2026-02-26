ffb8424999ccce158fc94ba8faf216c34d8ad7e6
nasti <anastasiasaienko634@gmail.com>
2026-02-26 20:23:03 +0100

    update everything

---
+++ components/ScrollToTopButton/ScrollToTopButton.tsx
@@ -0,0 +1,11 @@
+"use client";
+import React, { useEffect, useState } from "react";
+import css from "./ScrollToTopButton.module.css";
+// Icon arrow up
+import { IoArrowUpOutline } from "react-icons/io5";
+
+const ScrollToTopButton = () => {
+  return <></>;
+};
+
+export default ScrollToTopButton;
