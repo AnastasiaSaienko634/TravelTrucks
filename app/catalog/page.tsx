ff0e6a9afbd1db341e33c664fb5d9e8e25f9df60
nasti <anastasiasaienko634@gmail.com>
2026-03-01 13:48:52 +0100

    update everything

--- app/catalog/page.tsx
+++ app/catalog/page.tsx
@@ -1,20 +1,22 @@
 "use client";
-import CamperList from "@/components/CamperList/CamperList";
-import LocationDropdown from "@/components/LocationDropdown/LocationDropdown";
+import { useEffect, useState } from "react";
 import css from "./CatalogPage.module.css";
 import { useFilterStore } from "@/lib/store/campersStore";
-import { useEffect, useState } from "react";
 import { fetchCampersByFilter } from "@/lib/api/serverApi";
+// Components
 import Filters from "@/components/Filters/Filters";
-import ScrollToTopButton from "@/components/ScrollToTopButton/ScrollToTopButton";
+import CamperList from "@/components/CamperList/CamperList";
+import LocationDropdown from "@/components/LocationDropdown/LocationDropdown";
 
 const CatalogPage = () => {
+  // Location state
   const city = useFilterStore((state) => state.city);
   const vehicleTp = useFilterStore((state) => state.vehicleType);
   const vehicleEq = useFilterStore((state) => state.equipment);
   const [campers, setCampers] = useState([]);
 
   useEffect(() => {
+    // Fetch Campers
     const fetchCampers = async () => {
       const location = city.length > 0 ? city : "";
       const vehicleEquipment = vehicleEq ? vehicleEq : {};
